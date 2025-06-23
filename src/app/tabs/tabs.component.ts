d48803035daba14563eadc894bacf62ef97fab49
Caleb <2682726+calebd-anderson@users.noreply.github.com>
2025-06-23 06:09:42 -0500

    move tab model to model

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { itablink } from '../itablink';
+import { itablink } from '../model/itablink';
 
 @Component({
   selector: 'app-tabs',
