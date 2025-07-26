8ca3c2ff18fba2e123592c20c222c7362e3f3f65
Caleb Anderson <2682726+calebd-anderson@users.noreply.github.com>
2025-07-26 18:30:10 -0500

    better css flask animation

--- src/app/component/chemistry/experiment/experiment.component.ts
+++ src/app/component/chemistry/experiment/experiment.component.ts
@@ -1,27 +1,26 @@
-import {Component, inject, Input, OnInit} from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { Element } from '../../../model/element.model';
-import {Observable, Subscription} from "rxjs";
-import {CompoundService} from "../../../service/compound.service";
+import { Observable, Subscription } from "rxjs";
+import { CompoundService } from "../../../service/compound.service";
 import { AuthenticationService } from '../../../service/security/authentication.service';
-import {Reaction} from "../../../model/compound";
+import { Reaction } from "../../../model/compound";
 import { HttpErrorResponse, HttpResponse } from "@angular/common/http";
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { ValidationModalComponent } from "./validation-modal/validation-modal.component";
 import { NotificationService } from '../../../service/notification.service';
 import { NotificationType } from '../../../model/enum/notification-type.enum';
 import { FlaskComponent } from './flask/flask.component';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { CommonModule } from '@angular/common';
 import { ExperimentService } from 'src/app/service/experiment.service';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-    selector: 'app-experiment',
-    imports: [FlaskComponent, MatProgressBarModule, CommonModule, MatButtonModule],
-    templateUrl: './experiment.component.html',
-    styleUrls: ['./compound.component.scss'],
+  selector: 'app-experiment',
+  imports: [FlaskComponent, MatProgressBarModule, CommonModule, MatButtonModule],
+  templateUrl: './experiment.component.html',
+  styleUrls: ['./experiment.component.scss']
 })
-
 export class ExperimentComponent implements OnInit {
   private interacted: Boolean = false;
   private eventsSubscription: Subscription;
