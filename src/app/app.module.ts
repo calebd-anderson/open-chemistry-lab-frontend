8532de2087cfa34473235e54bc76e85d10fd2ec5
Caleb Anderson <2682726+calebd-anderson@users.noreply.github.com>
2025-08-15 17:17:05 -0500

    remove unused entities from app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {
-  HTTP_INTERCEPTORS,
-  provideHttpClient,
-  withInterceptorsFromDi,
-} from '@angular/common/http';
-
-// import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
-import { AuthInterceptor } from './service/security/interceptor/auth.interceptor';
-import { AuthenticationService } from './service/security/authentication.service';
-import { UserService } from './service/user.service';
-import { AuthenticationGuard } from './service/security/guard/authentication.guard';
-import { NotificationService } from './service/notification.service';
-// import { LoginComponent } from './component/user_manager/login/login.component';
-// import { RegisterComponent } from './component/user_manager/register/register.component';
 import { UserComponent } from './component/user_manager/user/user.component';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
@@ -25,30 +10,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { QuizComponent } from './component/game/quiz/quiz.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { ElementService } from './service/element.service';
-// import { TabsComponent } from './component/tabs/tabs.component';
 import { FlashcardComponent } from './component/game/flashcard/flashcard.component';
 import { ProfileComponent } from './component/user_manager/profile/profile.component';
-// import { ValidationModalComponent } from './component/chemistry/experiment/validation-modal/validation-modal.component';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { WelcomeComponent } from './component/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    // LoginComponent,
     UserComponent,
     QuizComponent,
-    // TabsComponent,
     UserComponent,
     FlashcardComponent,
     ProfileComponent,
-    // ValidationModalComponent,
   ],
   imports: [
     BrowserModule,
-    // AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
     MatCardModule,
@@ -60,16 +37,6 @@ import { WelcomeComponent } from './component/welcome/welcome.component';
     MatSidenavModule,
     MatProgressBarModule,
     WelcomeComponent,
-  ],
-  providers: [
-    MatSnackBar,
-    NotificationService,
-    AuthenticationGuard,
-    AuthenticationService,
-    ElementService,
-    UserService,
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-    provideHttpClient(withInterceptorsFromDi()),
   ],
 })
 export class AppModule {}
