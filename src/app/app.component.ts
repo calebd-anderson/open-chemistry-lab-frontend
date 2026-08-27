50581e4a37e6cea213dec102e0f68e4e1af7575a
Caleb Anderson <2682726+calebd-anderson@users.noreply.github.com>
2026-08-27 12:46:05 -0500

    extract main-header to its own component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,19 +5,16 @@ import {
   inject,
   signal,
 } from '@angular/core';
-import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { NotificationType } from './model/enum/notification-type.enum';
 import { User } from './model/user';
 import { AuthenticationService } from './service/security/authentication.service';
-import { AuthorizationService } from './service/security/authorization.service';
 import { NotificationService } from './service/notification.service';
 import { MatDialog } from '@angular/material/dialog';
 import { TabsComponent } from './component/tabs/tabs.component';
-import { LoginComponent } from './component/user_manager/login/login.component';
-import { ChemLogo as ChemLogo } from './logo.component';
-import { RegisterComponent } from './component/user_manager/register/register.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { UserNavComponent } from './component/user_manager/user-nav/user-nav.component';
+import { MainHeaderComponent } from './component/main-header/main-header.component';
 
 @Component({
   selector: 'app-root',
@@ -26,9 +23,9 @@ import { UserNavComponent } from './component/user_manager/user-nav/user-nav.com
   imports: [
     TabsComponent,
     RouterOutlet,
-    ChemLogo,
     FooterComponent,
-    UserNavComponent
+    UserNavComponent,
+    MainHeaderComponent,
   ],
 })
 export class AppComponent implements OnInit {
@@ -42,7 +39,6 @@ export class AppComponent implements OnInit {
     inject(NotificationService);
 
   public isLoggedIn: boolean = this.authenticationService.getIsLoggedIn();
-  public isMenuOpen: boolean = false;
 
   readonly dialog = inject(MatDialog);
   private readonly router = inject(Router);
@@ -54,27 +50,6 @@ export class AppComponent implements OnInit {
       let user: User = this.authenticationService.getUserFromLocalCache();
       this.user.set(user);
     }
-  }
-
-  openLogin() {
-    const dialogRef = this.dialog.open(LoginComponent);
-    dialogRef.afterClosed().subscribe((result) => {
-      if (typeof result == 'object') {
-        this.isLoggedIn = true;
-        this.user.set(result);
-      } else if (result === 'register') {
-        this.openRegister();
-      }
-    });
-  }
-
-  openRegister() {
-    const dialogRef = this.dialog.open(RegisterComponent);
-    dialogRef.afterClosed().subscribe((result) => {
-      if (result !== undefined) {
-        this.openLogin();
-      }
-    });
   }
 
   getLoggedIn(newItem: User) {
@@ -93,27 +68,6 @@ export class AppComponent implements OnInit {
         notificationType,
         'An error occured. Please try again.',
       );
-    }
-  }
-
-  public openMenu() {
-    const menu = document.getElementById('user-nav-menu');
-    if (menu) {
-      // Close the menu when clicking outside of it
-      const handleClickOutside = (event: MouseEvent) => {
-        const isClickInsideMenu = menu.contains(event.target as Node);
-        const isClickOnProfileImage = (event.target as Element).closest('.profileInfo');
-
-        if (!isClickInsideMenu && !isClickOnProfileImage) {
-          menu.classList.remove('active');
-          this.isMenuOpen = false;
-          document.removeEventListener('click', handleClickOutside);
-        }
-      };
-
-      // Toggle the menu
-      menu.classList.toggle('active');
-      console.log(menu.classList)
     }
   }
 }
---
+++ src/app/component/main-header/main-header.component.ts
@@ -0,0 +1,65 @@
+import { Component, inject, model } from '@angular/core';
+import { ChemLogo } from '../../logo.component';
+import { User } from '../../model/user';
+import { AuthenticationService } from '../../service/security/authentication.service';
+import { LoginComponent } from '../user_manager/login/login.component';
+import { MatDialog } from '@angular/material/dialog';
+import { RegisterComponent } from '../user_manager/register/register.component';
+
+@Component({
+  selector: 'app-main-header',
+  imports: [ChemLogo],
+  templateUrl: './main-header.component.html',
+  styleUrl: './main-header.component.scss',
+})
+export class MainHeaderComponent {
+  user = model<User | null>();
+  readonly dialog = inject(MatDialog);
+  public isMenuOpen: boolean = false;
+
+  public authenticationService: AuthenticationService = inject(
+    AuthenticationService,
+  );
+
+  openLogin() {
+    const dialogRef = this.dialog.open(LoginComponent);
+    dialogRef.afterClosed().subscribe((result) => {
+      if (typeof result == 'object') {
+        this.user.set(result);
+      } else if (result === 'register') {
+        this.openRegister();
+      }
+    });
+  }
+
+  openRegister() {
+    const dialogRef = this.dialog.open(RegisterComponent);
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result !== undefined) {
+        this.openLogin();
+      }
+    });
+  }
+
+  public openMenu() {
+    const menu = document.getElementById('user-nav-menu');
+    if (menu) {
+      // Close the menu when clicking outside of it
+      const handleClickOutside = (event: MouseEvent) => {
+        const isClickInsideMenu = menu.contains(event.target as Node);
+        const isClickOnProfileImage = (event.target as Element).closest(
+          '.profileInfo',
+        );
+
+        if (!isClickInsideMenu && !isClickOnProfileImage) {
+          menu.classList.remove('active');
+          this.isMenuOpen = false;
+          document.removeEventListener('click', handleClickOutside);
+        }
+      };
+
+      // Toggle the menu
+      menu.classList.toggle('active');
+    }
+  }
+}
