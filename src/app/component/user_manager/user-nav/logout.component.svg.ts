4b19aa829b66bc4d95bdb58e92fe6b539899c57f
Caleb Anderson <2682726+calebd-anderson@users.noreply.github.com>
2026-08-27 09:38:15 -0500

    user nav icon styles

--- src/app/component/user_manager/user-nav/logout.component.svg.ts
+++ src/app/component/user_manager/user-nav/logout.component.svg.ts
@@ -10,7 +10,7 @@ import { Component, input } from '@angular/core';
     >
       <path
         d="M15 4H18C19.1046 4 20 4.89543 20 6V18C20 19.1046 19.1046 20 18 20H15M8 8L4 12M4 12L8 16M4 12L16 12"
-        stroke="#000000"
+        stroke="currentColor"
         stroke-width="1.5"
         stroke-linecap="round"
         stroke-linejoin="round"
