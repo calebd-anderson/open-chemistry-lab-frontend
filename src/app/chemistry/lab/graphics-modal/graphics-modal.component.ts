71126aa969d573c0a0267489621e1d3860b95c6f
Caleb Anderson <2682726+calebd-anderson@users.noreply.github.com>
2026-09-13 21:12:11 -0500

    move graphics-modal under lab

--- src/app/chemistry/lab/graphics-modal/graphics-modal.component.ts
+++ src/app/chemistry/lab/graphics-modal/graphics-modal.component.ts
@@ -1,7 +1,12 @@
-import { Component, signal, Signal, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  signal,
+  Signal,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 import { MatDialogRef, MatDialogModule } from '@angular/material/dialog';
 import { ButtonComponent } from '@/app/component/button/button.component';
-import { D3FdgComponent } from "../d3-fdg/d3-fdg.component";
+import { D3FdgComponent } from '../experiment/d3-fdg/d3-fdg.component';
 import { ClusterMapResponse } from '@/app/model/clustmapresp.model';
 
 @Component({
@@ -15,14 +20,17 @@ export class GraphicsModalComponent {
   constructor(public dialogRef: MatDialogRef<GraphicsModalComponent>) {}
 
   // public discovery: string | undefined = '';
-  public confirmMessage: string = 'Let\'s see what the experiment reveals.';
+  public confirmMessage: string = "Let's see what the experiment reveals.";
   public wasSuccessful: string = 'Experiment complete';
   public isLoggedIn: string = '';
 
   public data = signal<ClusterMapResponse>({ nodes: [], links: [] });
 
   public get isSuccess(): boolean {
-    return this.wasSuccessful.toLowerCase().includes('congrat') || this.wasSuccessful.toLowerCase().includes('success');
+    return (
+      this.wasSuccessful.toLowerCase().includes('congrat') ||
+      this.wasSuccessful.toLowerCase().includes('success')
+    );
   }
 
   // public get hasDiscovery(): boolean {
--- src/app/chemistry/lab/lab.component.ts
+++ src/app/chemistry/lab/lab.component.ts
@@ -1,4 +1,11 @@
-import { Component, computed, inject, OnInit, signal, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  OnInit,
+  signal,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 import { PeriodicTableComponent } from '../periodic-table/periodic-table.component';
 import { ExperimentComponent } from './experiment/experiment.component';
 import { Element } from '@app/model/element.model';
@@ -12,7 +19,7 @@ import { ValidationModalComponent } from './experiment/validation-modal/validati
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Reaction } from '@app/model/compound';
 import { ElementRequest } from '@/app/model/element-request.model';
-import { GraphicsModalComponent } from './experiment/graphics-modal/graphics-modal.component';
+import { GraphicsModalComponent } from './graphics-modal/graphics-modal.component';
 import { ClusterMapResponse } from '@/app/model/clustmapresp.model';
 
 // import test_data from '../experiment/d3-fdg/h20_test_ml_graph.json';
